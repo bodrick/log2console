34998c6b0284223a8878ffdbdaf5de67624844bf
HansHammel <mail@specialsolutions.de>
2021-02-12 09:05:45 +0100

    package and .net update and blurry font fix

--- src/Log2Console/Program.cs
+++ src/Log2Console/Program.cs
@@ -6,6 +6,9 @@ namespace Log2Console
 {
     internal static class Program
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
         /// <summary>
         ///     The main entry point for the application.
         /// </summary>
@@ -14,6 +17,9 @@ namespace Log2Console
         {
             try
             {
+                if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 var mainForm = new MainForm();
